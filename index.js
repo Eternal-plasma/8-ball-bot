abae2494befb6e6b015307982e6f208675d5fc7f
Eternal_plasma <eternal.plasma1634@gmail.com>
2022-04-04 09:25:51 -0500

    Prevent Misuse

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const Discord = require('discord.js');
 var quotes = ['It is certain', 'It is decidedly so', 'Without a doubt', 'Yes – definitely', 'You may rely on it', 'As I see it yes', 'Most likely', 'Outlook good', 'Yes', 'Reply hazy, try again', 'Ask again later', 'Better not tell you now', 'Cannot predict now', 'Concentrate and ask again', 'Don’t count on it', 'My reply is no', 'My sources say no', 'Outlook not so good', 'Very doubtful']
 const bot = new Discord.Client();
-const token = 'put your bots token';
+const token = 'token_here';
 bot.login(token);
 bot.on('ready', () =>{
     console.log('This bot is on')
@@ -18,5 +18,12 @@ bot.on('message', msg=>{
     if (msg.content.endsWith(prefix))                                                                                      
     {                                                                                                                      
         msg.reply(randomQuote());                                                                                          
-    }                                                                                                                      
+    } 
+    if (msg.author.bot == false){
+        if (msg.content.includes("kill"))
+        {
+            msg.reply("My sources say there should be no killing of yourself or others, please call the suicide hotline 800-273-8255 to get help. You are loved");
+        }      
+    }
+                                                                                                                   
 });
